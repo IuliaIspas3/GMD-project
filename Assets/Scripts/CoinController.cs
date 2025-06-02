da96186f6dedb84978613933784b1c5f9629cf89
Iulia-Maria Ispas <119042517+IuliaIspas3@users.noreply.github.com>
2025-06-02 14:47:07 +0200

    add enemy to the main scene

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -4,13 +4,13 @@ public class CoinController : MonoBehaviour
 {
     public float rotationSpeed = 180f;
     private Vector3 rotationAxis;
-    public float lightIntensity = 1;
-    private Light light;
+    // public float lightIntensity = 1;
+    // private Light light;
     void Start()
     {
         rotationAxis = Vector3.up;
-        light = gameObject.GetComponent<Light>();
-        light.range = lightIntensity;
+        // light = gameObject.GetComponent<Light>();
+        // light.range = lightIntensity;
     }
 
     void Update()
