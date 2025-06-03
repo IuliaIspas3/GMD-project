4f2315eeea440978805eb7fc616c14d798971975
diegithyst <331289@viauc.dk>
2025-06-03 20:03:48 +0200

    Added coin and everything works

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -6,9 +6,11 @@ public class CoinController : MonoBehaviour
     public float rotationSpeed = 180f;
     private Vector3 rotationAxis;
     public static int coinCount = 0;
+    public AudioSource audioSource;
 
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         rotationAxis = Vector3.up;
         coinCount = 0;
     }
@@ -30,7 +32,9 @@ public class CoinController : MonoBehaviour
                  GameManager.coinText.text = coinCount.ToString();
              if (coinCount >= 10)
                  SceneManager.LoadScene("WinningScene");
-             Destroy(gameObject);
+             audioSource.Play();
+             Destroy(gameObject, audioSource.clip.length);
+
          }
      }
 
