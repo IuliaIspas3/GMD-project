4ee2cff396649d02d9a845af5c94e78ac4579307
diegithyst <331289@viauc.dk>
2025-06-03 13:05:36 +0200

    Added so many coins

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,14 +6,17 @@ public class EnemyController : MonoBehaviour
     public Transform character;
     private NavMeshAgent agent;
     private Animator animator;
+    private AudioSource audioSource;
 
     public float chaseRange = 15f;
     public float punchRange = 2f;
+    private bool hasPlayed = false;
     public float speed = 4f;
     
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        audioSource = GetComponent<AudioSource>();
         agent.speed = speed;
         animator = GetComponentInChildren<Animator>();
         
@@ -39,6 +42,11 @@ public class EnemyController : MonoBehaviour
 
         if (distance <= chaseRange)
         {
+            if (!hasPlayed)
+            {
+                audioSource.Play();
+                hasPlayed = true;
+            }
             if (distance <= punchRange)
             {
                 agent.ResetPath(); 
@@ -59,17 +67,18 @@ public class EnemyController : MonoBehaviour
                 animator.SetBool("isPunching", false);
 
                 Vector3 direction = (character.position - transform.position).normalized;
-                direction.y = 0; // Keep only horizontal rotation
+                direction.y = 0; 
                 if (direction != Vector3.zero)
                 {
                     Quaternion lookRotation = Quaternion.LookRotation(direction);
-                    transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f); // Smooth rotation
+                    transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
                 }
             }
         }
         else
         {
             agent.ResetPath();
+            hasPlayed = false;
             animator.SetBool("isWalking", false);
             animator.SetBool("isPunching", false);
         }
