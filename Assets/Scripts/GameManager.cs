f255eb4a91ae278b89f3a0b001bd775c4086a01e
Iulia-Maria Ispas <119042517+IuliaIspas3@users.noreply.github.com>
2025-06-03 13:05:46 +0200

    adding the commands

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     public static TextMeshProUGUI coinText;
-
     public TextMeshProUGUI coinTextReference;
+
+    public static TextMeshProUGUI controlsText;
+    public TextMeshProUGUI controlsTextReference;
 
     void Awake()
     {
         coinText = coinTextReference;
+        controlsText = controlsTextReference;
     }
     
     void Update()
@@ -20,5 +23,21 @@ public class GameManager : MonoBehaviour
         {
             SceneManager.LoadScene("IntroScene"); 
         }
+    }
+    
+    public static void SetMessage(string message, Color color)
+    {
+        if (controlsText != null)
+        {
+            controlsText.text = message;
+            controlsText.color = color;
+            Debug.LogWarning(message);
+        }
+    }
+
+    public static void ClearMessage()
+    {
+        if (controlsText != null)
+            controlsText.text = "";
     }
 }
